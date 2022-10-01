3e4125bfa1845c220a78477016f460995daad1dc
Alexandr <kitalex777@mail.ru>
2022-10-01 21:58:40 +0300

    initial commit

---
+++ src/conditionalStatements/Main.java
@@ -0,0 +1,47 @@
+package conditionalStatements;
+
+public class Main {
+
+    public static void main(String[] args) { task6();}
+
+    public static void task6() {
+        int age = 18;
+        int salary = 80_000;
+        if (age >= 23 && salary >=50_000 && salary <80_000){
+            System.out.println("Мы готовы вам выдать кредитную карту с лимитом " + (salary*3)*1.2 + " руб.");
+        } else if (age>= 23 && salary >= 80_000) {
+            System.out.println("Мы готовы вам выдать кредитную карту с лимитом " + (salary*3)*1.5 + " руб.");
+        }
+        else if (age <23 && salary >=50_000 && salary<80_000){
+            System.out.println("Мы готовы вам выдать кредитную карту с лимитом " + (salary*2)*1.2 + "руб");
+        }
+        else {System.out.println("Мы готовы вам выдать кредитную карту с лимитом " + (salary*2)*1.5 + " руб.");
+        }
+
+
+
+
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/variables/Main.java
@@ -0,0 +1,45 @@
+package variables;
+
+public class Main {
+
+
+    public static void main(String[] args){task8();}
+
+
+    public static void task6(){
+
+        System.out.println("Задача 1");
+        System.out.println();
+        int a = 12;
+        int b = 27;
+        int c = 44;
+        int d = 15;
+        int e = 9;
+
+        int result = a * (b + (c - d * e));
+        System.out.println(result * -1);
+
+    }
+
+    public static void task7(){
+
+        int a = 5;
+        int b = 7;
+        a = a + b - a;
+        b = (b+b+b+b+b)/a;
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+
+    public static void task8() {
+        int a = 125;
+        int b = (a % 100)/10;
+        System.out.println(b);
+
+    }
+
+
+
+}
+
